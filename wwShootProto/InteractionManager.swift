2f253b76f7256cc8ab07a56ba9b0ed4d94ad850c
Jakintosh <jak33@mac.com>
2014-10-25 13:22:21 -0400

    added outline for interaction data

--- wwShootProto/InteractionManager.swift
+++ wwShootProto/InteractionManager.swift
@@ -9,6 +9,58 @@
 import Foundation
 import SpriteKit
 
+// InteractionData
+//
+// Interaction Data stores all relevent information for a full interaction sequence. This
+// includes all possible moments in an interaction, and the information on how to navigate
+// through an interaction.
+
+struct InteractionData {
+    
+}
+
+
+// InteractionMoment
+//
+// An Interaction Moment is a single presentation of (non-)interactive bubbles. For example,
+// one moment may be the daughter saying something, and another might be a choice the father
+// has to make.
+
+struct InteractionMoment {
+    // what data must this contain?
+    //
+    // • character involved
+    // • all relevant dialogue for each character
+    // • all relevant animations for each character
+    // • all camera queues
+    // • next moment name
+    
+    // properties
+/*
+    var activeEntity: InteractiveEntity
+    var dialogueKey: String
+    var startAnimationKey: String?
+    var endAnimationKey: String?
+    var cameraAction: idfk
+    var nextMomentKey: String
+*/
+    
+    // what happens before
+    //
+    // • characters must be in position
+    //
+    // what happens during
+    //
+    // • dialogue is played
+    // • animations are played
+    // • camera is pan/zoomed
+    //
+    // what happens after
+    //
+    // • transitions to next moment
+    // • optionally plays new animations
+}
+
 class InteractionManager {
     
     // MARK: Properties
